542018d88538d137db7eb42ef0349f848624ebc6
Oyatulloh-Isroilov <oyatullohisroilov5@gmail.com>
2023-10-22 19:10:53 +0500

    4-oy 8-dars

---
+++ app.js
@@ -0,0 +1,130 @@
+//
+// 3-masala
+//
+
+// Man
+
+// function Man(Name, Surname, Age) {
+//   this.Name = Name;
+//   this.Surname = Surname;
+//   this.Age = Age;
+// }
+
+// Notebook
+
+// function Notebook(NotebookName, ProcessorName, VideocardName) {
+//   this.NotebookName = NotebookName;
+//   this.ProcessorName = ProcessorName;
+//   this.VideocardName = VideocardName;
+// }
+
+// Car
+
+// function Car(CarName, CarFactoryName, CarYear) {
+//   this.CarName = CarName;
+//   this.CarFactoryName = CarFactoryName;
+//   this.CarYear = CarYear;
+// }
+
+// example
+
+// function example(Number1, Number2, Number3, Number4, result) {
+//   this.Number1 = Number1;
+//   this.Number2 = Number2;
+//   this.Number3 = Number3;
+//   this.Number4 = Number4;
+//   this.result = Number1 + Number2 - Number3 + Number4;
+// }
+
+// Man
+// const Man1 = new Man("Oyatulloh", "Isroilov", 13);
+
+// Notebook
+// const Notebook1 = new Notebook(
+//   "Acer Aspire 3",
+//   "Intel Core I5-12",
+//   "Intel Iris Xe, NVIDIA mx550"
+// );
+
+// Car
+// const Car1 = new Car("BMW m5 Asco", "BMW", "2021-year");
+
+// example
+// const example1 = new example(24, 17, 35, 23);
+
+// console logs
+// console.log(Man1);
+// console.log(Notebook1);
+// console.log(Car1);
+// console.log(example1);
+
+//
+// 4-masala
+//
+
+// let a = +prompt("son kiriting");
+// let res;
+
+// if (a % 2 == 1) {
+//   res = a ** 3;
+//   console.log(res);
+// } else {
+//   res = a ** 2;
+//   console.log(res);
+// }
+
+// localStorage.setItem("resultat", res);
+
+//
+// 5-masala
+//
+
+// let users = `[
+//     {
+//         Number: 13
+//     },
+//     {
+//         Number: 21
+//     },
+//     {
+//         Number: 42
+//     }
+// ]`;
+
+// let user1 = JSON.parse(users);
+// console.log(user1);
+
+//
+// 6-masala
+//
+
+// let obj = JSON.parse('{"Number1":5, "Number2":13, "Number3":32}');
+
+// let max = Number1;
+
+// if (max < Number2) {
+//   max = Number2;
+//   console.log(max);
+// } else if (max < Number3) {
+//   max = Number3;
+// }
+// console.log(max);
+
+//
+//7-masala
+//
+
+// function block(Mashina, Notbuk, Kompyuter, Sichqoncha) {
+//   this.Mashina = Mashina;
+//   this.Notbuk = Notbuk;
+//   this.Kompyuter = Kompyuter;
+//   this.Sichqoncha = Sichqoncha;
+// }
+
+// let a = prompt("so'z kiriting");
+
+// if ((a = Mashina)) {
+//   console.log("to'g'ri so'z kiritingiz");
+// } else {
+//   console.log("Notog'ri so'z kiritingiz");
+// }
